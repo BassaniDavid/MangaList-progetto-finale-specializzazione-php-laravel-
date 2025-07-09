990363770749dfb646bef7f997de055c1b34b17d
BassaniDavid <davbassan98@gmail.com>
2025-07-09 11:32:51 +0200

    feat: Add DashboardController and update dashboard view to display total mangas and genres

---
+++ backoffice/app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Manga;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $totalMangas = Manga::count();
+        $totalGenres = Genre::count();
+
+        return view('dashboard', compact('totalMangas', 'totalGenres'));
+    }
+}
--- backoffice/resources/views/dashboard.blade.php
+++ backoffice/resources/views/dashboard.blade.php
@@ -19,6 +19,22 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+
+            </div>
+            <div class="jumbotron p-5 mb-4 bg-light rounded-3">
+                <div class="container py-5">
+                    <h1 class="fs-2 text-center">Benvenuto nella dashboard!</h1>
+                    <p class="lead">Qui puoi vedere un riepilogo dei tuoi manga e generi.</p>
+                    <hr class="my-4">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2>Totale Manga: {{ $totalMangas }}</h2>
+                        </div>
+                        <div class="col-md-6">
+                            <h2>Totale Generi: {{ $totalGenres }}</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
