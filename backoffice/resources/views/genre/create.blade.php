71ab100cc067e48671e72ba5262bdff55697037c
BassaniDavid <davbassan98@gmail.com>
2025-07-10 11:57:15 +0200

    feat: Enhance create forms for Genre and Manga with improved styling and layout adjustments

--- backoffice/resources/views/genre/create.blade.php
+++ backoffice/resources/views/genre/create.blade.php
@@ -2,19 +2,21 @@
 
 @section('content')
 
-    <form action="{{ route('genre.store') }}" class="d-flex card p-4" method="POST">
+<div class="container my-5">
+    <form action="{{ route('genre.store') }}" class="card p-4" method="POST">
         @csrf
 
-        <label for="genere" class="my-2">genere</label>
+        <label for="genere" class="my-2 fs-4">genere</label>
         <input type="text" name="genere" id="genere">
 
-        <label for="descrizione" class="my-2">descrizione</label>
-        <textarea name="descrizione" id="descrizione" rows="5"></textarea>
+        <label for="descrizione" class="my-2 fs-4">descrizione</label>
+        <textarea name="descrizione" id="descrizione" rows="3"></textarea>
 
         {{-- manga --}}
-        <div class="mt-3  d-flex flex-wrap">
+        <div class="mt-3 d-flex row flex-wrap mb-3">
+            <span class="w-100 fs-4 mb-3">manga:</span>
                 @foreach ($mangas as $manga)
-                <div>
+                <div class="col-12 col-md-6 col-lg-4 col-xxl-3">
                     <input type="checkbox" class="ms-5" id="manga-{{ $manga->id }}" name="mangas[]" value="{{ $manga->id }}"></input>
                     <label for="manga-{{ $manga->id }}">{{$manga->titolo }}</label>
                 </div>
@@ -23,5 +25,8 @@
 
         <input type="submit" value="salva" class="btn btn-success mt-3 w-50 m-auto">
     </form>
+
+</div>
+
 
 @endsection
--- backoffice/resources/views/manga/create.blade.php
+++ backoffice/resources/views/manga/create.blade.php
@@ -5,45 +5,46 @@
 
 <div class="container my-5">
     <h1 class="text-center my-4">inserisci i dati del nuovo manga</h1>
-    <form action="{{ route('manga.store') }}" class="d-flex card p-4" method="POST">
+    <form action="{{ route('manga.store') }}" class="card p-4" method="POST">
         @csrf
 
-        <label for="titolo" class="my-2">titolo</label>
+        <label for="titolo" class="my-2 fs-4">titolo</label>
         <input type="text" name="titolo" id="titolo">
 
-        <label for="autore" class="my-2">autore</label>
+        <label for="autore" class="my-2 fs-4">autore</label>
         <input type="text" name="autore" id="autore">
 
-        <label for="editore" class="my-2">editore</label>
+        <label for="editore" class="my-2 fs-4">editore</label>
         <input type="text" name="editore" id="editore">
 
-        <label for="url_copertina" class="my-2">url_copertina</label>
+        <label for="url_copertina" class="my-2 fs-4">url_copertina</label>
         <input type="text" name="url_copertina" id="url_copertina">
 
-        <label for="status" class="my-2">status</label>
+        <label for="status" class="my-2 fs-4">status</label>
         <input type="text" name="status" id="status">
 
-        <label for="numero_volumi" class="my-2">numero_volumi</label>
+        <label for="numero_volumi" class="my-2 fs-4">numero_volumi</label>
         <input type="text" name="numero_volumi" id="numero_volumi">
 
-        <label for="data_inizio" class="my-2">data_inizio</label>
+        <label for="data_inizio" class="my-2 fs-4">data_inizio</label>
         <input type="text" name="data_inizio" id="data_inizio">
 
-        <label for="valutazione" class="my-2">valutazione</label>
-        <input type="text" name="valutazione" id="valutazione">
+        <label for="valutazione" class="my-2 fs-4">valutazione</label>
+        <input type="number" name="valutazione" id="valutazione" min="1" max="10">
 
 
         {{-- generi --}}
-        <div class="mt-3 d-flex flex-wrap">
+        <div class="mt-3 d-flex row flex-wrap mb-3">
+            <span class="w-100 fs-4 mb-3">generi:</span>
                 @foreach ($genres as $genre)
-                <div>
-                    <input type="checkbox" class="ms-3" id="genre-{{ $genre->id }}" name="genres[]" value="{{ $genre->id }}"></input>
+                <div  class="col-6 col-md-4 col-lg-3 col-xxl-2">
+                    <input type="checkbox" id="genre-{{ $genre->id }}" name="genres[]" value="{{ $genre->id }}"></input>
                     <label for="genre-{{ $genre->id }}">{{$genre->genere }}</label>
                 </div>
                 @endforeach
         </div>
 
-        <label for="descrizione" class="my-2">descrizione</label>
+        <label for="descrizione" class="my-2 fs-4">descrizione</label>
         <textarea name="descrizione" id="descrizione" rows="5"></textarea>
 
         <input type="submit" value="salva" class="btn btn-success mt-3 w-50 m-auto">
