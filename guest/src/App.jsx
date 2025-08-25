a775746b00c737d3ac601fda22a364fcc2cf382f
BassaniDavid <davbassan98@gmail.com>
2025-08-25 11:48:06 +0200

    refactor: remove unused useState import and related state for cleaner code

--- guest/src/App.jsx
+++ guest/src/App.jsx
@@ -1,16 +1,12 @@
 // importo destrutturando i componenti per le rotte da react-router-dom (dopo aver installato il pacchetto)
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { useState } from "react";
-
 // importo le pagine
 import ShowPage from "../pages/ShowPage";
 import DefaultLayout from "../layouts/DefaultLayouts";
 import IndexPage from "../pages/IndexPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>
